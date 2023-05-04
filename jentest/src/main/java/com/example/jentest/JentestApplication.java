22f6559e82b0e97aaad1bc2fa5b8ffba4e04fa12
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-04 15:14:19 +0000

    jenkins rool

---
+++ jentest/src/main/java/com/example/jentest/JentestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JentestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JentestApplication.class, args);
+    }
+
+}
---
+++ jentest/src/main/java/com/example/jentest/web/RestController.java
@@ -0,0 +1,13 @@
+package com.example.jentest.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/")
+   public String getMessage(){
+       return "jenkins start .....";
+   }
+}
